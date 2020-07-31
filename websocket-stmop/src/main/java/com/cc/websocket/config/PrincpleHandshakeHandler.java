1b214a2b11d14b1dca786a66f1bdf3faa108e7cf
13027730830 <wangchen1@zhongguancun.com.cn>
2020-07-31 14:54:26 +0800

    添加注释

--- websocket-stmop/src/main/java/com/cc/websocket/config/PrincpleHandshakeHandler.java
+++ websocket-stmop/src/main/java/com/cc/websocket/config/PrincpleHandshakeHandler.java
@@ -22,6 +22,14 @@ import java.util.Map;
 @Component
 public class PrincpleHandshakeHandler extends DefaultHandshakeHandler {
 
+    /**
+     * 根据请求的url携带的参数来定义Principal.Principal是在WebSocketSession中。存的是 user属性。
+     * 调用 simpMessagingTemplate.convertAndSendToUser("qq","/topic/notice",value) 会寻找session中的principal.getName()来确定user。
+     *
+     * @param [request, wsHandler, attributes]
+     * @author wangchen
+     * @createDate 2020/7/31
+     **/
     @Override
     protected Principal determineUser(ServerHttpRequest request, WebSocketHandler wsHandler, Map<String, Object> attributes) {
         //是否携带认证 token
